7fce99a320738e8b9fc2fc5c4b4693602087975a
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2023-08-09 00:42:28 -0300

    fuel supply

---
+++ schemas/fuelSupply.ts
@@ -0,0 +1,28 @@
+import { Rule } from "sanity";
+
+export default {
+  name: 'fuelSupply',
+  type: 'document',
+  title: 'Fuel supply',
+  fields: [
+    {
+      name: 'pricePerLiter',
+      type: 'number',
+      title: 'Price per liter',
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    {
+      name: 'price',
+      type: 'number',
+      title: 'Price',
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    {
+      name: 'car',
+      type: 'reference',
+      title: 'Car',
+      validation: (Rule: Rule) => Rule.required(),
+      to: [{type: 'car'}],
+    },
+  ],
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,6 @@
 import car from "./car";
+import fuelSupply from "./fuelSupply";
 import ride from "./ride";
 import trip from "./trip";
 
-export const schemaTypes = [car, ride, trip]
+export const schemaTypes = [car, ride, trip, fuelSupply]
