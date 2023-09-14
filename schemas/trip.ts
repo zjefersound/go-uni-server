b8635409e164c64ffb97b84f48fc954deb511871
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2023-09-14 11:01:39 -0300

    update trip

--- schemas/trip.ts
+++ schemas/trip.ts
@@ -1,3 +1,5 @@
+import { Rule } from "sanity";
+
 export default {
   name: 'trip',
   type: 'document',
@@ -7,21 +9,32 @@ export default {
       name: 'from',
       type: 'string',
       title: 'From',
+      validation: (Rule: Rule) => Rule.required(),
     },
     {
       name: 'to',
       type: 'string',
       title: 'To',
+      validation: (Rule: Rule) => Rule.required(),
     },
     {
       name: 'distance',
       type: 'number',
       title: 'Distance',
+      validation: (Rule: Rule) => Rule.required(),
     },
     {
       name: 'duration',
       type: 'number',
       title: 'Average duration in minutes',
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    {
+      name: 'user',
+      type: 'reference',
+      title: 'User',
+      validation: (Rule: Rule) => Rule.required(),
+      to: [{type: 'user'}],
     },
   ],
 }
